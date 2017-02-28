5b58d8d6f7b39b1771054823953b58b45da4b48f
Louie Rootfield <lourfield@gmail.com>
2017-03-01 05:21:12 +0700

    Updating script to match the tutorial

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -20,41 +20,28 @@ jQuery( function( $ ) {
   };
 
   /**
-   * Run when the Youtube video state (play, pause, etc.) change.
+   * Run when the Youtube video state (play, pause, etc.) is changed.
    *
    * @param {Object} event The Youtube Object Event.
    * @return {Void}
    */
   function onPlayerStateChange( event ) {
 
-    var isPlay = 1 === event.data;
+    var isPlay  = 1 === event.data;
     var isPause = 2 === event.data;
-    var isEnd = 0 === event.data;
+    var isEnd   = 0 === event.data;
 
-    toggleVideoClasses( isPlay, isPause, isEnd );
-  }
-
-  /**
-   * Toggle class names of the video iframe.
-   *
-   * @param {Boolean} play  Whether the video is playing.
-   * @param {Boolean} pause Whether the video is paused.
-   * @param {Boolean} end   Whather the video ends.
-   * @return {Void}
-   */
-  function toggleVideoClasses( play, pause, end ) {
-
-    if ( play ) {
+    if ( isPlay ) {
       $featuredVideo.removeClass( "is-paused" );
       $featuredVideo.toggleClass( "is-playing" );
     }
 
-    if ( pause ) {
+    if ( isPause ) {
       $featuredVideo.removeClass( "is-playing" );
       $featuredVideo.toggleClass( "is-paused" );
     }
 
-    if ( end ) {
+    if ( isEnd ) {
       $featuredVideo.removeClass( "is-playing", "is-paused" );
     }
   }
@@ -71,59 +58,73 @@ jQuery( function( $ ) {
    * @param {Object}   options Some options.
    * @return {Function} The function to execute.
    */
-  function throttle( func, wait, options ) {
+  // function throttle( func, wait, options ) {
+  //
+  //   var context, args, result;
+  //   var timeout = null;
+  //   var previous = 0;
+  //   if ( !options ) {
+  //     options = {};
+  //   }
+  //
+  //   var later = function() {
+  //     previous = options.leading === false ? 0 : Date.now();
+  //     timeout = null;
+  //     result = func.apply( context, args );
+  //     if ( !timeout ) {
+  //       context = args = null;
+  //     }
+  //   };
+  //
+  //   return function() {
+  //     var now = Date.now();
+  //     if ( !previous && options.leading === false ) {
+  //       previous = now;
+  //     }
+  //     var remaining = wait - ( now - previous );
+  //     context = this;
+  //     args = arguments;
+  //     if ( remaining <= 0 || remaining > wait ) {
+  //       if ( timeout ) {
+  //         clearTimeout( timeout );
+  //         timeout = null;
+  //       }
+  //       previous = now;
+  //       result = func.apply( context, args );
+  //       if ( !timeout ) {
+  //         context = args = null;
+  //       }
+  //     } else if ( !timeout && options.trailing !== false ) {
+  //       timeout = setTimeout( later, remaining );
+  //     }
+  //     return result;
+  //   };
+  // };
 
-    var context, args, result;
-    var timeout = null;
-    var previous = 0;
-    if ( !options ) {
-      options = {};
-    }
-
-    var later = function() {
-      previous = options.leading === false ? 0 : Date.now();
-      timeout = null;
-      result = func.apply( context, args );
-      if ( !timeout ) {
-        context = args = null;
-      }
-    };
-
-    return function() {
-      var now = Date.now();
-      if ( !previous && options.leading === false ) {
-        previous = now;
-      }
-      var remaining = wait - ( now - previous );
-      context = this;
-      args = arguments;
-      if ( remaining <= 0 || remaining > wait ) {
-        if ( timeout ) {
-          clearTimeout( timeout );
-          timeout = null;
-        }
-        previous = now;
-        result = func.apply( context, args );
-        if ( !timeout ) {
-          context = args = null;
-        }
-      } else if ( !timeout && options.trailing !== false ) {
-        timeout = setTimeout( later, remaining );
-      }
-      return result;
-    };
-  };
+  // $window
+  //
+  //   .on( "resize", throttle( function() {
+  //     top = $featuredMedia.offset().top;
+  //     offset = Math.floor( top + ( $featuredMedia.outerHeight() / 2 ) );
+  //   }, 150 ) )
+  //
+  //   .on( "scroll", throttle( function() {
+  //     $featuredVideo.toggleClass( "is-sticky",
+  //       $window.scrollTop() > offset && $featuredVideo.hasClass( "is-playing" )
+  //     );
+  //   }, 150 ) );
+  //
 
   $window
 
-    .on( "resize", throttle( function() {
+    .on( "resize", function() {
       top = $featuredMedia.offset().top;
       offset = Math.floor( top + ( $featuredMedia.outerHeight() / 2 ) );
-    }, 150 ) )
+    } )
 
-    .on( "scroll", throttle( function() {
+    .on( "scroll", function() {
       $featuredVideo.toggleClass( "is-sticky",
         $window.scrollTop() > offset && $featuredVideo.hasClass( "is-playing" )
       );
-    }, 150 ) );
+    } );
 } );
